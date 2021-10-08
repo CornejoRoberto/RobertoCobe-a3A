eddb1538273571a15bd6bc21432c9d7336706966
CobeñaCornejoRoberto <roberto_samp@outlook.com>
2021-10-08 17:30:02 -0500

    Agregar archivos de proyecto.

---
+++ RobertoCobeña3A/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RobertoCobeña3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mayor = 0; int num1 = 0; int con = 0;
+            Console.WriteLine("Escriba la cantidad de números a comparar");
+               int numeros = int.Parse(Console.ReadLine());
+               for (int i = 1; i <= numeros; i++)
+               {
+               con = con + 1;
+               Console.WriteLine("Escriba su número #" + con);
+               num1 = int.Parse(Console.ReadLine());
+                  if (i == 1)
+                  {
+                   mayor = num1;
+                  }
+                if (num1 > mayor) { mayor = num1; }              
+                }
+            Console.WriteLine("El número mayor es:" + mayor);
+                }
+	    }
+	 }
